d5c517eeda8bb8a7b0fe37708469b1a21f3f6419
Dilmurod_Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2024-05-28 23:37:02 +0500

    Task 2 done

--- Introduction_HomeTask/Controllers/CategoriesController.cs
+++ Introduction_HomeTask/Controllers/CategoriesController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Introduction_HomeTask.Models;
 using Introduction_HomeTask.Services;
+using Introduction_HomeTask.ViewModels;
+using System.Collections;
+using Humanizer.Bytes;
 
 namespace Introduction_HomeTask.Controllers
 {
@@ -26,6 +29,67 @@ namespace Introduction_HomeTask.Controllers
             _logger.LogInformation("Categories controller - Index");
             var listOfCategories  = _unitOfWork.Categories.GetAll();
             return View(listOfCategories);
+        }
+
+        //[Produces("image/jpeg", "application/json")]
+        public IActionResult GetCategoryImage(int id) 
+        {
+            byte[] imageData = _unitOfWork.Categories.GetById(id).Picture;
+
+            string contentType = "image/jpeg";
+
+            if (imageData is null) 
+            {
+                return NotFound();
+            }
+            return File(imageData, contentType);
+        }
+
+        public string GetCategoryImageAsSrc(int id)
+        {
+            byte[] imageData = _unitOfWork.Categories.GetById(id).Picture;
+
+            string imreBase64Data = Convert.ToBase64String(imageData);
+            string imgDataURL = string.Format("data:image/png;base64,{0}", imreBase64Data);
+            return imgDataURL;
+        }
+
+
+        public IActionResult EditPicture(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = _unitOfWork.Categories.GetById((int)id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(new CategoryPictureViewModel { Id = category.CategoryId});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EditPictureAsync(CategoryPictureViewModel categoryPicture)
+        {
+            if (categoryPicture.File != null )
+            {
+                using (var dataStream = new MemoryStream())
+                {
+                    await categoryPicture.File.CopyToAsync(dataStream);
+
+                    var category = _unitOfWork.Categories.GetById(categoryPicture.Id);
+                    if (category == null)
+                    {
+                        return NotFound();
+                    }
+                    category.Picture = dataStream.ToArray();
+                    _unitOfWork.Complete();
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Introduction_HomeTask/ViewModels/CategoryPictureViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Introduction_HomeTask.ViewModels
+{
+    public class CategoryPictureViewModel
+    {
+        public int Id { get; set; }
+        public IFormFile? File { get; set; }
+    }
+}
